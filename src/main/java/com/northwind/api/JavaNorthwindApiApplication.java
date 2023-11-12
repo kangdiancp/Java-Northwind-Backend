b30c0e216bb097422ac111409a1b78443d2e9ef4
kangdiancp <kangdiancp@gmail.com>
2023-11-12 17:40:44 +0700

    init project

---
+++ src/main/java/com/northwind/api/JavaNorthwindApiApplication.java
@@ -0,0 +1,13 @@
+package com.northwind.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNorthwindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNorthwindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northwind/api/JavaNorthwindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northwind.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNorthwindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
